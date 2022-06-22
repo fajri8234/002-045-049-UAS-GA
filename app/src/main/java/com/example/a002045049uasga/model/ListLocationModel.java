5eb3a9c504023a9b6e8d323384101c4e370eb45b
fajri8234 <95693790+fajri8234@users.noreply.github.com>
2022-06-22 22:05:41 +0700

    002 045 049 UAS GA

---
+++ app/src/main/java/com/example/a002045049uasga/model/ListLocationModel.java
@@ -0,0 +1,18 @@
+package com.example.a002045049uasga.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ListLocationModel {
+    @SerializedName("data")
+    private List<LocationModel> mData;
+
+    public ListLocationModel(List<LocationModel> mData) {this.mData = mData; }
+
+    public ListLocationModel(){
+    }
+    public List<LocationModel> getmData() { return mData;}
+
+    public void setmData(List<LocationModel> mData) {this.mData = mData;}
+}
